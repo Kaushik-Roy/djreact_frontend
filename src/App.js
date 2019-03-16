0e5eb81f41807395f7ddc9e6ebb426e330cb61a8
kaushik <roy.kaushikkumar1@gmail.com>
2019-03-16 17:32:00 +0530

    used redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
 import BaseRouter from './Routes';
-
 import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
-
 import CustomLayout from './containers/Layout';
+import * as actions from './store/action/auth';
 
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Router>
-          <CustomLayout>
+          <CustomLayout {...this.props}>
             <BaseRouter />
           </CustomLayout>
         </Router>
@@ -21,4 +26,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  }  
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }  
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import ArticleList from './containers/ArticleList';
 import ArticleDetail from './containers/ArticleDetail';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+	
 
 const BaseRouter = () => (
 
 	<div>
 		<Route exact path='/' component={ArticleList} />
-		<Route exact path='/:articleID' component={ArticleDetail} />
+		<Route exact path='/articles/:articleID/' component={ArticleDetail} />
+		<Route exact path='/login/' component={Login} />
+		<Route exact path='/signup/' component={Signup} />
 	</div>
 );
 
